233d8c1d9e2cf58f0d35531ed480f1bcb3b43d40
Yanis <yanis@netkoders.com>
2022-03-22 12:03:08 +0100

    vue

---
+++ assets/appVue.js
@@ -0,0 +1,1 @@
+console.log('Hello ! this is my vue test for Feedback - Self !')
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('appVue', './assets/appVue.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -54,6 +55,8 @@ Encore
         config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
+
+    .enableVueLoader()
 
     // enables Sass/SCSS support
     //.enableSassLoader()
